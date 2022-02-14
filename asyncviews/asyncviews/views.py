3c881e746ee4467e8ccfa23159e8dbd4fcb68142
bruno <bruno.mcreal@gmail.com>
2022-02-14 13:42:30 -0300

    Create new async

--- asyncviews/asyncviews/views.py
+++ asyncviews/asyncviews/views.py
@@ -13,6 +13,16 @@ async def http_call_async():
         print(r)
 
 
+async def http_call_async2():
+    element_list = ["one", "two", "tree", "four", "five"]
+    for element in element_list:
+        await asyncio.sleep(2)
+        print(element)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org")
+        print(r)
+
+
 def http_call_sync():
     for num in range(1, 6):
         sleep(1)
@@ -24,6 +34,7 @@ def http_call_sync():
 async def async_view(request):
     loop = asyncio.get_event_loop()
     loop.create_task(http_call_async())
+    loop.create_task(http_call_async2())
     return HttpResponse('Non-blocking HTTP request')
 
 
